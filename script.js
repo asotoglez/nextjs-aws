180446b7a44cc84b10ac59cb6c31ee9a6d2f7b1f
Alejandro Soto Gonzalez <alesoto9113@gmail.com>
2020-12-20 13:54:38 -0300

    Testing action artifact

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-const { exec } = require("child_process");
+const exec = require("child_process").exec;
 
 module.exports = ({ github, context }) => {
-  const { execSync } = require('child_process');
-  // stderr is sent to stdout of parent process
-  // you can set options.stdio if you want it to go elsewhere
-  const stdout = execSync('npx serverless');
-  const { spawnSync} = require('child_process');
-  const child = spawnSync('ls', );
-  console.error('error', child.error);
-  console.log('stdout ', child.stdout);
-  console.error('stderr ', child.stderr);
-  return child.stdout
-  // exec("npx serverless", (err, stdout, stderr) => {
-  //   if (err) {
-  //     //some err occurred
-  //     console.error(err);
-  //   } else {
-  //     // the *entire* stdout and stderr (buffered)
-  //     console.log(`stdout: ${stdout}`);
-  //     console.log(`stderr: ${stderr}`);
-  //     return stdout
-  //   }
-  // });
+  const myShellScript = exec("npx serverless");
+  let i = 0;
+  myShellScript.stdout.on("data", (data) => {
+    console.log(data, i);
+    i++;
+    return data
+  });
+  myShellScript.stderr.on("data", (data) => {
+    console.error(data);
+  });
 };
