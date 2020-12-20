404354f0e6d2c0f409d31404e3e1060e7effbe93
Alejandro Soto Gonzalez <alesoto9113@gmail.com>
2020-12-20 14:20:33 -0300

    Testing action artifact

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = async ({ github, context }) => {
   try {
     const { stdout, stderr } = await exec('npx serverless');
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
     return stdout
 }catch (err) {
    console.error(err);
