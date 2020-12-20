f205cafa440bfcb7d94efabc02f2649c62fe7bcd
Alejandro Soto Gonzalez <alesoto9113@gmail.com>
2020-12-20 14:15:48 -0300

    Testing action artifact

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const exec = require("child_process").exec;
-
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 module.exports = async ({ github, context }) => {
-  const myShellScript = exec("npx serverless");
-  let i = 0;
-  let res = ''
-  await myShellScript.stdout.on("data", (data) => {
-    res = res + data
-  });
-  myShellScript.stderr.on("data", (data) => {
-    console.error(data);
-  });
-  return res
+  try {
+    const { stdout, stderr } = await exec('npx serverless');
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+    return stdout
+}catch (err) {
+   console.error(err);
+};
 };
