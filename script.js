63fb41c5508e76079f60636bbede021bb170f761
Alejandro Soto Gonzalez <alesoto9113@gmail.com>
2020-12-20 13:34:47 -0300

    Testing action artifact

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-module.exports = ({github, context}) => {
-  return context
-}
+const { exec } = require("child_process");
+
+module.exports = ({ github, context }) => {
+  exec("npx serverless", (err, stdout, stderr) => {
+    if (err) {
+      //some err occurred
+      console.error(err);
+    } else {
+      // the *entire* stdout and stderr (buffered)
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    }
+  });
+  return context;
+};
