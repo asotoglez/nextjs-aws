8f5bf862e6570f180d291a0dfae9deb5f4fff104
Alejandro Soto Gonzalez <alesoto9113@gmail.com>
2020-12-20 15:32:31 -0300

    Testing action artifact

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ module.exports = async ({ github, context }) => {
   try {
     const { stdout, stderr } = await exec('npx serverless');
     const urlRegex = /(https?:\/\/[^ ]*)/;
-    return input.match(urlRegex)[1]
+    input.match(urlRegex)[1];
+    return stdout.match(urlRegex)[1]
 }catch (err) {
    console.error(err);
 };
